fc4ab89a86f0201c175a18c11340a074eedca92e
motorcycle54 <pierox57332@gmail.com>
2022-07-13 17:26:34 -0500

    cambios

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -23,19 +23,33 @@ export default {
   {
       let message = ref([])
       let lista_dialog = inject("lista_dialog");
+      let path = "lib/PDFJS/web/viewer.html"; //path of the PDF.js viewer.html
+      var configuracion_ventana = 'width=600,height=600,top=500,left=500,toolbar=no,location=no,status=no,menubar=no';
       window.addEventListener('message', event => {
       // IMPORTANT: check the origin of the data!
       if (event.origin.startsWith('http://localhost:8080')) {
           // The data was sent from your site.
           // Data sent with postMessage is stored in event.data:
-          lista_dialog.value.push(...event.data)
+          console.log(event.data)
+          let fileName = event.data[event.data.length-1].urlPdf
+          //lista_dialog.value.push(...event.data)
+          let dir = ref(path + "?file=" + encodeURIComponent(fileName));
+          console.log(dir.value)
+          window.open(dir.value,"Visor PDF",configuracion_ventana).focus();
+          //windowOpen(dir.value);
+          
       } else {
           
           return;
       }
-});
-    
-      return {message,lista_dialog}
+   
+      });
+      
+
+
+  
+
+      return {message,lista_dialog,configuracion_ventana,path}
   }
 }
 </script>
