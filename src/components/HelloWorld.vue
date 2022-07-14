90f2709f1a6b7c7f4091df5c4a37c6cca0c077a5
motorcycle54 <pierox57332@gmail.com>
2022-07-14 11:49:55 -0500

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,9 @@
-<template>
-  
-</template>
+<template></template>
 
 <script>
 import { ref } from "vue";
 import { DocumentRequestModel } from "../_models/documentRequest.model";
-import { ListDialogService } from '../_services/ListDialog.service';
+import { ListDialogService } from "../_services/ListDialog.service";
 import {
   inject,
   getCurrentInstance,
@@ -13,43 +11,34 @@ import {
   watchEffect,
 } from "@vue/runtime-core";
 export default {
-   
-
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-      message: String
+    message: String,
   },
-  setup(props)
-  {
-      let message = ref([])
-      let lista_dialog = inject("lista_dialog");
-      let path = "lib/PDFJS/web/viewer.html"; //path of the PDF.js viewer.html
-      var configuracion_ventana = 'width=600,height=600,top=500,left=500,toolbar=no,location=no,status=no,menubar=no';
-      window.addEventListener('message', event => {
+  setup(props) {
+    let message = ref([]);
+    let lista_dialog = inject("lista_dialog");
+    let path = "lib/PDFJS/web/viewer.html"; //path of the PDF.js viewer.html
+    var configuracion_ventana =
+      "_blank,width=600,height=600,top=500,left=500,toolbar=no,location=no,status=no,menubar=no";
+    window.addEventListener("message", (event) => {
       // IMPORTANT: check the origin of the data!
-      if (event.origin.startsWith('http://localhost:8080')) {
-          // The data was sent from your site.
-          // Data sent with postMessage is stored in event.data:
-          console.log(event.data)
-          let fileName = event.data[event.data.length-1].urlPdf
-          //lista_dialog.value.push(...event.data)
-          let dir = ref(path + "?file=" + encodeURIComponent(fileName));
-          console.log(dir.value)
-          window.open(dir.value,"Visor PDF",configuracion_ventana).focus();
-          //windowOpen(dir.value);
-          
-      } else {
-          
-          return;
-      }
-   
-      });
-      
+      // if (event.origin.startsWith("http://localhost:8080")) {
+      //   // The data was sent from your site.
+      //   // Data sent with postMessage is stored in event.data:
+      //   //windowOpen(dir.value);
+      // } else {
+      //   return;
+      // }
+      console.log(event.origin);
+      let fileName = event.data.urlPdf;
+      //lista_dialog.value.push(...event.data)
+      let dir = ref(path + "?file=" + encodeURIComponent(fileName));
+      console.log(dir.value);
+      window.open(dir.value, "_blank", configuracion_ventana).focus();
+    });
 
-
-  
-
-      return {message,lista_dialog,configuracion_ventana,path}
-  }
-}
+    return { message, lista_dialog, configuracion_ventana, path };
+  },
+};
 </script>
